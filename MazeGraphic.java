fac1d03a830db5b34644fc85f25e3150e97e4f7a
SleeknFoxy <sjjurasek@hotmail.com>
2017-01-11 13:58:08 -0500

    Create MazeGraphic.java
    I had pre-existing code from before so I implemented this into the repository.
    class MazeGraphic is intended to be for the processing of the visuals of the game.

---
+++ MazeGraphic.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MazeGraphic extends JPanel {
+	
+	int jpanW, jpanH;
+	int blockX, blockY;
+	
+	MazeGraphic(){
+		this.setBackground(Color.DARK_GRAY);
+	}
+	
+	void initGraphics(){
+		jpanW = this.getSize().width;
+		jpanH = this.getSize().height;
+		
+		blockX = (int)((jpanW/TheMain.GRID)+0.5);
+		blockY = (int)((jpanH/TheMain.GRID)+0.5);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		initGraphics();
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		
+		g2.setStroke(new BasicStroke(2.0f));
+		
+		for (int i=0;i<TheMain.GRID;i++) {
+			for (int j=0;j<TheMain.GRID;j++) {
+				if (TheMain.board[i][j] == 0) {
+					
+					g2.setColor(new Color(65,164,65));
+					g2.fillRect(blockX*i,blockY*j, blockX, blockY);
+					
+				}
+			}
+		}
+	}
+}
