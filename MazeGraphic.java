fc08e48a82281e9e9447216d1545d3f292fc54c1
SleeknFoxy <sjjurasek@hotmail.com>
2017-01-24 14:41:43 -0500

    more code

--- MazeGraphic.java
+++ MazeGraphic.java
@@ -1,18 +1,21 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class MazeGraphic extends JPanel {
+public class MazeGraphic extends JPanel{
 	
 	int jpanW, jpanH;
 	int blockX, blockY;
 	
 	MazeGraphic(){
 		this.setBackground(Color.DARK_GRAY);
+		this.setFocusable(true);
+		initGraphics();
 	}
 	
 	void initGraphics(){
-		jpanW = this.getSize().width;
-		jpanH = this.getSize().height;
+		jpanH = TheMain.GRID*TheMain.PIXPERSQ;
+		jpanW = jpanH;
 		
 		blockX = (int)((jpanW/TheMain.GRID)+0.5);
 		blockY = (int)((jpanH/TheMain.GRID)+0.5);
@@ -21,16 +24,26 @@ public class MazeGraphic extends JPanel {
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		initGraphics();
+		
 		Graphics2D g2 = (Graphics2D) g;
+		Composite originalComp = g2.getComposite();
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 		
 		g2.setStroke(new BasicStroke(2.0f));
 		
+		double dist = 0;
+		int x = TheMain.person.x;
+		int y = TheMain.person.y;
 		for (int i=0;i<TheMain.GRID;i++) {
 			for (int j=0;j<TheMain.GRID;j++) {
-				if (TheMain.board[i][j] == TheMain.SOLID) {
+				dist = Math.sqrt((i-x)*(i-x) + (j-y)*(j-y));
+				
+				// TODO make it so that the edge of the view has low opacity
+				if(dist > 5.5){
+					g2.setColor(Color.BLACK);
+					g2.fillRect(blockX*i,blockY*j, blockX, blockY);
+				}else if (TheMain.board[i][j] == TheMain.SOLID) {
 					g2.setColor(new Color(65,164,65));
 					g2.fillRect(blockX*i,blockY*j, blockX, blockY);
 				} else if(TheMain.board[i][j] == TheMain.START){
@@ -40,7 +53,18 @@ public class MazeGraphic extends JPanel {
 					g2.setColor(new Color(130,180,255));
 					g2.fillRect(blockX*i,blockY*j, blockX, blockY);
 				}
+				
+				if(dist > 3.5 && dist <= 4.5){
+					g2.setColor(new Color(0,0,0,133));
+					g2.fillRect(blockX*i,blockY*j, blockX, blockY);
+				} else if(dist > 4.5&& dist<= 5.5){
+					g2.setColor(new Color(0,0,0,200));
+					g2.fillRect(blockX*i,blockY*j, blockX, blockY);
+				}
 			}
 		}
+		int num = (int)(TheMain.PIXPERSQ)/4;
+		g2.setColor(Color.WHITE);
+		g2.drawOval(x * blockX + num, y * blockY + num, TheMain.person.width, TheMain.person.width);
 	}
 }
